739370dac651881bf82f386da6a78fe13a550baa
elvismaicol <elvismaicol@gmail.com>
2020-01-04 21:54:24 -0300

    Programa String

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -9,17 +9,17 @@ public class Program {
         
         Scanner sc = new Scanner(System.in);
         
-        PrintServices<Integer> ps = new PrintServices();
+        PrintServices<String> ps = new PrintServices();
         
         System.out.print("How many values?");
         int n = sc.nextInt();
         
         for (int i = 0; i < n; i++) {
-            int value = sc.nextInt();
+            String value = sc.next();
             ps.addValue(value);
         }
         ps.print();
-        Integer x = ps.first();
+        String x = ps.first();
         System.out.println("First: " + x);
         
         sc.close();
